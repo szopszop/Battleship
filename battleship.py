8e6cf06c90871965911cdf040c4631e65d154e9c
Szymon Tracz <szymontracz1@gmail.com>
2022-03-02 08:37:57 +0100

    working placement phase

--- battleship.py
+++ battleship.py
@@ -1,43 +1,81 @@
-
+from copy import deepcopy
+import os, time
 
 height = 5
 width = 5
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = '0123456789'
-ship_size = 3
-size = 5
+ship_ship = 3
+ship = 5
+
+def console_clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def get_difficulty(): # extra
     pass
 
 
-def create_game_board(size) -> list:
+def create_game_board(ship) -> list:
     board = []
-    for i in range(size):
-        board.append(['o'] * size)
+    for i in range(ship):
+        board.append(['o'] * ship)
     return board
 
-def print_board(board, size):
+
+def cover_board(board):
+    covered_board = []
+    for row in board:
+        covered_row = []
+        for l in row:
+            covered_row.append(str(l))
+        covered_board.append(covered_row)
+    return covered_board
+
+
+# def print_board(board):
+#     column_names = [' ']
+#     for i in range(len(board[0])):
+#         column_names.append(alphabet[i])
+
+#     covered_board = cover_board(board)
+    
+#     print('')
+#     print(' '.join(column_names))
+#     row_number = 1
+#     for row in covered_board:
+#         print(f'{row_number} {" ".join(row)}')
+#         row_number += 1
+#     return covered_board
+
+def print_board(board):
     char = 0
     i = 1
     h = []
-    while (i <= size):
+    while (i <= height):
         h.append(str(i))
         i = i + 1
     x = ' '.join(h)
     # print(h)
     print("")
     print('        ' + x)
-    print('        ' + '↓ ' * size)
-    print('       ' + '__' * size)
+    print('        ' + '↓ ' * width)
+    print('       ' + '__' * width)
     for element in board:
         char += 1
         element = ' '.join(element)
         print(chr(char + 64).rjust(2), "→ ", "|", element, "|")
         print("")
-    print('       ' + '--' * size)
+    print('       ' + '--' * width)
     return board
+
+
+def display_boards(display_board_1, display_board_2):
+    print('\nPlayer 1       Player 2')
+    print('  1 2 3 4 5      1 2 3 4 5')
+    for i in range(len(display_board_1)):
+        print(chr(65+i)+ ' '+' '. join(display_board_1[i])+'    '+chr(65+i)+ ' '+' '. join(display_board_2[i]))
+    print()
 
 
 def get_field_position(height, width):
@@ -54,100 +92,134 @@ def get_field_position(height, width):
         return row, column
 
 
-def print_board():
-    pass
-
-
-def place_ship(game_board): # work in progress ~Sebastian
-    size = 0
+def check_valid_position(board, row, column) -> bool:
+    if row >= height or row < 0 or column >= width or column < 0:
+        return True
+    elif board[row][column] == 'X':
+        return False
     
-    while size != ship_size:
-        row, column = get_field_position(height, width)
-        try:
-            if game_board[row - 1 : row + 2][column] == 'X' or game_board[row][column -1 : column + 2] == 'X':
-                print("Ships cant be touching (except at the corners)")
-                continue
-        except IndexError:
-            pass
-
-        game_board[row][column] = 'X'
+    return True
 
 
-def user_move(game_board_1: list, game_board_2: list) -> tuple:
-    is_empty_flag = False
-    no_collision_flag = False
-
-    while not (is_empty_flag and no_collision_flag):
-        is_empty_flag = False
-        no_collision_flag = False
-
-        position_x, position_y = get_field_position(height, width)
-        
-        
-        if game_board_1[position_x][position_y] == 0:
-            is_empty_flag = True
-        if game_board_2[position_x][position_y] == 0:
-            is_empty_flag = True  
-        else:
-            print('\nField already taken.')
-            continue
-
-        
-
-
-        if is_empty_flag and no_collision_flag:
-            return position_x, position_y
-
+def check_for_neighbours(board, row, column) -> bool:
+    try:
+        if board[row+1][column] == 'X':
+            return False
+        elif board[row-1][column] == 'X':
+            return False
+        elif board[row][column+1] == 'X':
+            return False        
+        elif board[row][column-1] == 'X':
+            return False
+        return True
+    except IndexError:
+        print('index error')
 
 
 def ask_for_ship_orientation():
     direction = input("Horizontal or Vertical? [H/W]").upper()
     while not (direction == 'H' or direction == 'W'):
         direction = input("Horizontal or Vertical? [H/W]").upper()
-    if direction == 'H': return True
-    else: return False
+    if direction == 'H': 
+        return False
+    return True
 
 
-
-def player_1_start_deployment_procedure():
-      print(f'Player 1 deploys ships.')
-
-      print_board()
-
-      # petla rozmieszczania statkow
-      ship_sizes = [2,1]
-      for current_size in ship_sizes: ## dla kazdego z rozmiarow statkow w tablicy ship_sizes
-          move_acceptable=False        ## zaloz ze ruch nie jest dopuszczalny
-          while not move_acceptable:
-               print(f'Select position for a ship of {current_size} size: ')
-               start_position = user_move(current_size)   # pobierz pozycje startowa
-               is_horizontal = ask_for_ship_orientation()        # pobierz orientacje statku
-               move_acceptable = is_move_allowed(start_position,is_horizontal)  # sprawdz czy ruch jest akceptowalny
-               
-               if move_acceptable:
-                    place_ship(start_position, is_horizontal) # zapisz na tablicy
-                    print("Ship placed successfully.")       # wyswietl komunikat
-                    print_board()
-               else:
-                    print("Not possible.")            # poinformuj o bledzie i rozpocznij while od nowa
-
-                    
-# def is_move_allowed():
-#     return True
+# def place_ship(board): # work in progress, only places 3 ship 1 ships ~Sebastian
+#     game_board = deepcopy(board)
+#     ships = [2, 1]
+#     for ship in ships:
+#         current_ship = 0
+#         while current_ship != ship:
+#             if ship == 0:
+#                 row, column = get_field_position(height, width) 
+#                 if check_valid_position(game_board, row, column) and check_valid_position(game_board, row-1, column) and check_valid_position(game_board, row+1, column) and check_valid_position(game_board, row, column - 1) and check_valid_position(game_board, row, column + 1):
+#                     game_board[row][column] = 'X'
+#                     print_board(game_board)
+#                     current_ship += 1
+#                 else:
+#                     print_board(game_board)
+#                     print("\nInvalid position! Try some place else")
+#             else:
+#                 orientation = ask_for_ship_orientation(ship)
+#                 print("This will be the part of the ship closest to top-left corner: ")
+#                 row, column = get_field_position(height, width) 
+#                 if orientation:
+#                     pass
+#     print("\nDone placing.")
 
 
-# def game_logic():
-#     game_board_1 = create_game_board()
-#     game_board_2 = create_game_board()
-
-
-
-# def main():
-#     game_logic()
+def validation(game_board) -> tuple:
+    valid_flag, neighbours_flag = False, False
     
+    while not (valid_flag and neighbours_flag):
+        valid_flag, neighbours_flag = False, False
+
+        row, col = get_field_position(height, width)
+        if check_valid_position(game_board, row, col):
+            valid_flag = True
+        else:
+            print('Invalid move.')
+            continue
+        if check_for_neighbours(game_board, row, col):
+            neighbours_flag = True
+        else:
+            print('Invalid move.')
+            continue
+        
+        if valid_flag and neighbours_flag:
+            return row, col
+
+def placing_ships(game_board, row, col, ship):
+    if ship == 1:
+        game_board[row][col] = 'X'
+        print_board(game_board)
+
+    if ship > 1:
+        if ask_for_ship_orientation():          # True = horizontal
+            for i in range(ship):
+                game_board[row+i][col] = 'X'
+        else:                                   # False = vertical
+            for i in range(ship):
+                game_board[row][col+i] = 'X'
+
+                
+def game_setup() -> list:
+    game_board_1 = create_game_board(ship)
+    game_board_2 = create_game_board(ship)
+    display_board_1 = deepcopy(game_board_1)
+    display_board_2 = deepcopy(game_board_2)
+    return game_board_1, game_board_2, display_board_1, display_board_2
 
 
-# if __name__ == "__main__":
-#     main()
-# create_game_board(size)
-# print_board([["o","o","o","o","o"],["o","o","o","o","o"],["o","o","o","o","o"],["o","o","o","o","o"], ["o","o","o","o","o"]], size)
+def placement_phase():
+    print('\nWelcome to the Battleship game!')
+    game_board_1, game_board_2, display_board_1, display_board_2 = game_setup()
+    print_board(game_board_1)
+
+    ships = [3, 2, 1]
+    # Player_1
+    print('Player 1 turn.')
+    for ship in ships:
+        print(f'Choose place for a ship of size {ship}.')
+        row, col = validation(game_board_1)
+        placing_ships(game_board_1, row, col, ship)
+        print_board(game_board_1)
+    # Player_2
+    print('\nPlayer 2 turn.\n')
+    for ship in ships:
+        print(f'Choose place for a ship of size {ship}.')
+        row, col = validation(game_board_2)
+        placing_ships(game_board_2, row, col, ship)
+        print_board(game_board_2)
+
+    console_clear()
+    display_boards(display_board_1, display_board_2)
+
+
+def main():
+    placement_phase()
+
+
+if __name__ == "__main__":
+     main()
